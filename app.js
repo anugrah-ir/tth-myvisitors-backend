d3b921f8cca2b9d18c5a56e77eb2980c1a8ae050
Anugrah Ilhami Rizki <anugrahilhamirizki@gmail.com>
2024-07-29 22:59:43 +0700

    feat: add database connection using Sequelize ORM

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./src/config/database')
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,17 @@ app.get('/', (req, res) => {
     res.send('success');
 });
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+app
+    .listen(port, () => {
+        console.log(`App listening on port ${port}`);
+    })
+    .on('error', (error) => {
+        console.error('Error starting the server:', error);
+    });
